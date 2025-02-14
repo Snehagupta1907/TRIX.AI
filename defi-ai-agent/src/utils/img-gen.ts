3800e0625f4cec3d14851fceb67e8a2abda6ab6f
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-02-14 14:55:11 +0530

    update -nft

--- defi-ai-agent/src/utils/img-gen.ts
+++ defi-ai-agent/src/utils/img-gen.ts
@@ -6,12 +6,10 @@ import axios from 'axios';
 export const generateImage = async (prompt: string): Promise<string | null> => {
     const apiUrl = process.env.VENICE_API_BASE_URL;
     const apiKey = process.env.VENICE_API_KEY; // Secure API Key Handling
-
     if (!apiKey || !apiUrl) {
         console.error("Error: API key or URL is missing.");
         throw new Error("Venice api key or URL is missing.");
     }
-
     const requestBody = {
         model_name: "SDXL1.0-base",
         prompt: prompt,
@@ -19,7 +17,6 @@ export const generateImage = async (prompt: string): Promise<string | null> => {
         width: 1024,
         backend: "auto"
     };
-
     try {
         const response = await axios.post(apiUrl, requestBody, {
             headers: {
@@ -34,12 +31,9 @@ export const generateImage = async (prompt: string): Promise<string | null> => {
             console.error("Error: No valid image data received from API", response.data);
             return null;
         }
-
         const base64Image = response.data.images[0].image;
-
         // Convert Base64 to Buffer
         const imageBuffer = Buffer.from(base64Image, 'base64');
-
         // Upload the image buffer to IPFS
         const ipfsUrl = await uploadImageToIPFS(imageBuffer);
         const nftObj={
