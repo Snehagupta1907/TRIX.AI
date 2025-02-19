bf199597c0fe5d031cb8a5b3f8e01d615e075009
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-02-19 22:50:49 +0530

    layer zero config file

---
+++ trix-layerzero/deployment_config.ts
@@ -0,0 +1,29 @@
+export const deploymentConfig = {
+    84532: {
+        network: 'base-sepolia',
+        chainId: 84532,
+        zeUSDAddress: '0x4801E23D9aBeA9e18538eBF95631BE0724eC148f',
+        adapterAddress: '0xA99bFF98db7A93A40935D85E5F5A2D4E99CF47d8',
+        oft_address: '',
+        lzEndpointOnSrcChain: '0x6EDCE65403992e310A62460808c4b910D972f10f',
+        lzEndpointId: '40245',
+    },
+    57054: {
+        network: 'sonic-blaze-testnet',
+        chainId: 57054,
+        zeUSDAddress: '',
+        adapterAddress: '',
+        oft_address: '0xBFff78BB02925E4D8671D0d90B2a6330fcAedd87',
+        lzEndpointOnSrcChain: '0x6C7Ab2202C98C4227C5c46f1417D81144DA716Ff',
+        lzEndpointId: '40349',
+    },
+    17000: {
+        network: 'holesky',
+        chainId: 17000,
+        zeUSDAddress: '',
+        adapterAddress: '',
+        oft_address: '0x72105396D6b1c1378581D5Be21683A6470c1F2aE',
+        lzEndpointOnSrcChain: '0x6EDCE65403992e310A62460808c4b910D972f10f',
+        lzEndpointId: '40217',
+    },
+}
